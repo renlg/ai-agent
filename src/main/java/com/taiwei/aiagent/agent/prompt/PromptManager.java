92062833c4f50b6eb58f70ebf4ee83b34a56c572
renlinggao <771623370@qq.com>
2026-07-08 11:46:49 +0800

    refactor: 使用 Velocity 模板渲染系统提示词，去除重复项目信息

--- src/main/java/com/taiwei/aiagent/agent/prompt/PromptManager.java
+++ src/main/java/com/taiwei/aiagent/agent/prompt/PromptManager.java
@@ -2,61 +2,55 @@ package com.taiwei.aiagent.agent.prompt;
 
 import com.intellij.openapi.project.Project;
 import com.taiwei.aiagent.settings.AiAgentSettings;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import java.io.File;
+import java.io.StringWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Properties;
 
 /**
  * Prompt 模板管理器
- * 生成系统提示词，告知 Agent 其身份和可用工具
+ * 使用 Velocity 模板引擎渲染系统提示词
  */
 public class PromptManager {
 
     private final Project project;
-    private final TemplateEngine templateEngine;
+    private final VelocityEngine velocityEngine;
 
     public PromptManager(Project project) {
         this.project = project;
-        this.templateEngine = new TemplateEngine();
+        this.velocityEngine = createVelocityEngine();
+    }
+
+    private VelocityEngine createVelocityEngine() {
+        Properties props = new Properties();
+        props.setProperty("resource.loaders", "classpath");
+        props.setProperty("resource.loader.classpath.class", ClasspathResourceLoader.class.getName());
+        VelocityEngine engine = new VelocityEngine();
+        engine.init(props);
+        return engine;
     }
 
     /**
      * 生成系统提示词
      */
     public String buildSystemPrompt() {
-        String projectName = project.getName();
-        String basePath = project.getBasePath();
+        VelocityContext context = new VelocityContext();
 
-        Map<String, Object> context = new HashMap<>();
-        context.put("projectName", projectName);
-        context.put("basePath", basePath != null ? basePath : "\u672a\u77e5");
-
-        String systemPrompt = templateEngine.render("templates/system_prompt.vm", context);
-        return systemPrompt + "\n" + buildEnvBlock();
-    }
-
-    private String buildEnvBlock() {
         String basePath = project.getBasePath();
         String workingDir = basePath != null ? basePath : "未知";
+        context.put("workingDir", workingDir);
 
         boolean isGitRepo = basePath != null && new File(basePath, ".git").exists();
+        context.put("isGitRepo", isGitRepo ? "Yes" : "No");
 
-        String osName = System.getProperty("os.name", "").toLowerCase();
-        String platform;
-        if (osName.contains("mac") || osName.contains("darwin")) {
-            platform = "macos";
-        } else if (osName.contains("linux")) {
-            platform = "linux";
-        } else if (osName.contains("win")) {
-            platform = "windows";
-        } else {
-            platform = osName;
-        }
-
-        String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+        context.put("platform", detectPlatform());
+        context.put("today", LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
 
         String model;
         try {
@@ -64,13 +58,24 @@ public class PromptManager {
         } catch (Exception e) {
             model = "未知";
         }
+        context.put("model", model != null ? model : "未知");
 
-        return "<env>\n"
-                + "  Working directory: " + workingDir + "\n"
-                + "  Is directory a git repo: " + (isGitRepo ? "Yes" : "No") + "\n"
-                + "  Platform: " + platform + "\n"
-                + "  Today's date: " + today + "\n"
-                + "  Model: " + model + "\n"
-                + "</env>";
+        Template template = velocityEngine.getTemplate("templates/system_prompt.vm", "UTF-8");
+        StringWriter writer = new StringWriter();
+        template.merge(context, writer);
+        return writer.toString();
+    }
+
+    private String detectPlatform() {
+        String osName = System.getProperty("os.name", "").toLowerCase();
+        if (osName.contains("mac") || osName.contains("darwin")) {
+            return "macos";
+        } else if (osName.contains("linux")) {
+            return "linux";
+        } else if (osName.contains("win")) {
+            return "windows";
+        } else {
+            return osName;
+        }
     }
 }
