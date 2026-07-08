9cfc6ef29d00b8c86b7d3d66822ce551a54e5f79
renlinggao <771623370@qq.com>
2026-07-08 11:36:39 +0800

    feat: 在系统提示词中注入 <env> 环境信息块

--- src/main/java/com/taiwei/aiagent/agent/prompt/PromptManager.java
+++ src/main/java/com/taiwei/aiagent/agent/prompt/PromptManager.java
@@ -1,7 +1,11 @@
 package com.taiwei.aiagent.agent.prompt;
 
 import com.intellij.openapi.project.Project;
+import com.taiwei.aiagent.settings.AiAgentSettings;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,6 +34,43 @@ public class PromptManager {
         context.put("projectName", projectName);
         context.put("basePath", basePath != null ? basePath : "\u672a\u77e5");
 
-        return templateEngine.render("templates/system_prompt.vm", context);
+        String systemPrompt = templateEngine.render("templates/system_prompt.vm", context);
+        return systemPrompt + "\n" + buildEnvBlock();
+    }
+
+    private String buildEnvBlock() {
+        String basePath = project.getBasePath();
+        String workingDir = basePath != null ? basePath : "未知";
+
+        boolean isGitRepo = basePath != null && new File(basePath, ".git").exists();
+
+        String osName = System.getProperty("os.name", "").toLowerCase();
+        String platform;
+        if (osName.contains("mac") || osName.contains("darwin")) {
+            platform = "macos";
+        } else if (osName.contains("linux")) {
+            platform = "linux";
+        } else if (osName.contains("win")) {
+            platform = "windows";
+        } else {
+            platform = osName;
+        }
+
+        String today = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+        String model;
+        try {
+            model = AiAgentSettings.getInstance().getModel();
+        } catch (Exception e) {
+            model = "未知";
+        }
+
+        return "<env>\n"
+                + "  Working directory: " + workingDir + "\n"
+                + "  Is directory a git repo: " + (isGitRepo ? "Yes" : "No") + "\n"
+                + "  Platform: " + platform + "\n"
+                + "  Today's date: " + today + "\n"
+                + "  Model: " + model + "\n"
+                + "</env>";
     }
 }
